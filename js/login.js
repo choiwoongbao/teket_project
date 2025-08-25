105efe08f4d5232270c67b85a15e20217314a4b3
cgoiwoongbao <jengeun3692@gmail.com>
2025-08-25 18:02:15 +0900

    8/25일 작업물 완료

--- js/login.js
+++ js/login.js
@@ -1,27 +1,48 @@
 window.addEventListener("load", function () {
-  // 각각의 요소를 변수에 담아놓는다.
   const nav = document.querySelector(".menu-list");
   const headerBg = document.querySelector(".header-bg");
-   const searchBox = document.querySelector(".search-box");
-
-  // nav에 마우스 엔터 했을때 즉 호버했을때
-  nav.addEventListener("mouseenter", function () {
-    headerBg.style.height = "70px";
+  const navItems = nav.querySelectorAll(".menu-list > li");
+  
+  navItems.forEach((item, index) => {
+    // li 안쪽 전체 영역에 마우스 들어올 때
+    item.addEventListener("mouseenter", function (e) {
+      // 3번째 메뉴만 헤더 배경 보이게
+      if (index <= 2) {
+        headerBg.style.height = "70px";
+        headerBg.style.backgroundColor = "#1e73be";
+      } else {
+        headerBg.style.height = "0px";
+        headerBg.style.backgroundColor = "transparent";
+      }
+    });
   });
-nav.addEventListener("mouseover", function (e) {
-    const target = e.target.closest("li");
 
-    if (!target) return;
-       if (!target.querySelector(".search-box")) {
-      headerBg.style.height = "70px";
+  // nav 벗어나면 배경 숨김
+  nav.addEventListener("mouseleave", function () {
+    headerBg.style.height = "0px";
+    headerBg.style.backgroundColor = "transparent";
+  });
+
+  // 언어 클릭했을때
+  const langBtn = this.document.querySelector(".language-icon");
+  const krus = this.document.querySelector(".kr-us");
+  langBtn.addEventListener("click", function (e) {
+    e.preventDefault(); // a태그라면 새로고침 막기
+
+    const current = window.getComputedStyle(krus).display;
+
+    if (current === "none") {
+      krus.style.display = "flex";
     } else {
-      headerBg.style.height = "0px";
+      krus.style.display = "none";
     }
   });
 
-  // nav에 마우스 리브 했을때 즉 호버를 안했을때
-  nav.addEventListener("mouseleave", function () {
-    headerBg.style.height = "0px";
+  // 햄버거바 클릭 시
+  const hamburger = this.document.querySelector(".hamburgerBtn");
+  const mobile = this.document.querySelector(".mobile-menu");
+
+  hamburger.addEventListener("click", function () {
+    mobile.classList.toggle("show");
   });
 });
-
